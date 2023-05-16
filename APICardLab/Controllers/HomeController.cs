0ceceadedb2bda60debfc35806a40a1ba913135b
daniellarsen77 <daniellarsen127@gmail.com>
2023-05-15 23:37:36 -0400

    Finished Lab

---
+++ APICardLab/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using APICardLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using APICardLab.Models;
+using Unit_7_Deck_Of_Cards_API_Lab.Models;
+
+namespace APICardLab
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DeckDAL deckAPI = new DeckDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deck deck = deckAPI.NewDeck();
+            List<Card> drawnCards = deckAPI.DrawCard(deck.deck_id).cards.ToList();
+            return View(drawnCards);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ APICardLab/Models/DeckDAL.cs
@@ -0,0 +1,25 @@
+﻿using APICardLab.Models;
+using RestSharp;
+
+namespace Unit_7_Deck_Of_Cards_API_Lab.Models
+{
+    public class DeckDAL
+    {
+        public Deck NewDeck()
+        {
+            string URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            RestClient client = new RestClient(URL);
+            RestRequest request = new RestRequest();
+            Deck response = client.Get<Deck>(request);
+            return response;
+        }
+        public CardResults DrawCard(string DeckID)
+        {
+            string URL = $"https://deckofcardsapi.com/api/deck/{DeckID}/draw/?count=5";
+            RestClient client = new RestClient(URL);
+            RestRequest request = new RestRequest();
+            CardResults response = client.Get<CardResults>(request);
+            return response;
+        }
+    }
+}
